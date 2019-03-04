4d6a690dfd7cdd27316d6e0d729dee3d38bfffeb
Phonekham <phone.madridista@gmail.com>
2019-03-04 22:52:35 +0700

    coding challenge 3

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,20 @@
+function tipCalculator(bill) {
+  var percentage;
+  if (bill < 50) {
+    percentage = .2;
+  } else if (bill >= 50 && bill < 200) {
+    percentage = .15;
+  } else {
+    percentage = .1;
+  }
+  return percentage * bill;
+}
+// console.log(tipCalculator(100));
+var bills = [55, 100, 300];
+var tips = [tipCalculator(bills[0]),
+  tipCalculator(bills[2])
+];
+var finalvalue = [bills[0] + tips[0],
+  bills[2] + tips[2]
+];
+console.log(tips, finalvalue);
