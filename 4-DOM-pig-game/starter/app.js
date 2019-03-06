26b9870c3e1fcfd8b6d2b5de92721cd24657db10
Phonekham <phone.madridista@gmail.com>
2019-03-06 21:20:03 +0700

    8. Updating Scores and Changing the Active Player

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -12,9 +12,7 @@ var scores, roundScore, activePlayer, dice;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
-
-// document.querySelector('#current-' + activePlayer).innerHTML = "<em>" + dice + "</em>";
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = "none";
 document.getElementById('score-0').textContent = '0';
@@ -22,10 +20,22 @@ document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
 
-document.querySelector('btn-roll').addEventListener('click', function() {
+document.querySelector('.btn-roll').addEventListener('click', function() {
   dice = Math.floor(Math.random() * 6) + 1;
   // /Display the result
   var diceDOM = document.querySelector('.dice');
   diceDOM.style.display = "block";
-  diceDOM.src = 'dice-' + dice;
-})
+  diceDOM.src = 'dice-' + dice + '.png';
+
+  if (dice !== 1) {
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.add('active');
+  }
+});
