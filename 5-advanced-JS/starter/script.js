e356c74de156d10695b37afac0e15027ad64f13a
Phonekham <phone.madridista@gmail.com>
2019-03-07 20:55:05 +0700

    4. Creating Objects Function Constructors

---
+++ 5-advanced-JS/starter/script.js
@@ -0,0 +1,22 @@
+// function constructor
+var phone = {
+  name: 'Phone',
+  yearOfBirth: 1995,
+  job: 'Web Developer'
+};
+
+var Person = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+}
+
+Person.prototype.calculateAge = function() {
+  console.log(2019 - this.yearOfBirth);
+}
+
+var phone = new Person('Phonekham', 1995, "Developer");
+var phone2 = new Person('Phonekham2', 1994, "Developer");
+console.log(phone);
+phone.calculateAge();
+phone2.calculateAge();
