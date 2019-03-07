5b2a6d60db5f3def41ade9c90f64e4a09a2b0b43
Phonekham <phone.madridista@gmail.com>
2019-03-07 22:28:43 +0700

    primative vs object

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -22,23 +22,39 @@
 // phone2.calculateAge();
 
 // object.create
-var personProto = {
-  calculateAge: function() {
-    console.log(2019 - this.yearOfBirth);
-  }
+// var personProto = {
+//   calculateAge: function() {
+//     console.log(2019 - this.yearOfBirth);
+//   }
+// };
+// var phone = Object.create(personProto);
+// phone.name = 'phone';
+//
+// var test = Object.create(personProto, {
+//   name: {
+//     value: 'phone'
+//   },
+//   yearOfBirth: {
+//     value: 1995
+//   },
+//   job: {
+//     value: 'Developer'
+//   }
+//
+// });
+
+// primative vs object
+var a = 23;
+var b = a,
+  a = 46;
+console.log(a);
+console.log(b);
+
+var obj1 = {
+  name: 'phone',
+  age: 24
 };
-var phone = Object.create(personProto);
-phone.name = 'phone';
-
-var test = Object.create(personProto, {
-  name: {
-    value: 'phone'
-  },
-  yearOfBirth: {
-    value: 1995
-  },
-  job: {
-    value: 'Developer'
-  }
-
-});
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
