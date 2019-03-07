ddc9bde82c9c0d4e89289325da8d923563324afe
Phonekham <phone.madridista@gmail.com>
2019-03-07 21:31:19 +0700

    6. Creating Objects Object.create

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,22 +1,44 @@
 // function constructor
-var phone = {
-  name: 'Phone',
-  yearOfBirth: 1995,
-  job: 'Web Developer'
+// var phone = {
+//   name: 'Phone',
+//   yearOfBirth: 1995,
+//   job: 'Web Developer'
+// };
+//
+// var Person = function(name, yearOfBirth, job) {
+//   this.name = name;
+//   this.yearOfBirth = yearOfBirth;
+//   this.job = job;
+// }
+//
+// Person.prototype.calculateAge = function() {
+//   console.log(2019 - this.yearOfBirth);
+// }
+//
+// var phone = new Person('Phonekham', 1995, "Developer");
+// var phone2 = new Person('Phonekham2', 1994, "Developer");
+// console.log(phone);
+// phone.calculateAge();
+// phone2.calculateAge();
+
+// object.create
+var personProto = {
+  calculateAge: function() {
+    console.log(2019 - this.yearOfBirth);
+  }
 };
+var phone = Object.create(personProto);
+phone.name = 'phone';
 
-var Person = function(name, yearOfBirth, job) {
-  this.name = name;
-  this.yearOfBirth = yearOfBirth;
-  this.job = job;
-}
+var test = Object.create(personProto, {
+  name: {
+    value: 'phone'
+  },
+  yearOfBirth: {
+    value: 1995
+  },
+  job: {
+    value: 'Developer'
+  }
 
-Person.prototype.calculateAge = function() {
-  console.log(2019 - this.yearOfBirth);
-}
-
-var phone = new Person('Phonekham', 1995, "Developer");
-var phone2 = new Person('Phonekham2', 1994, "Developer");
-console.log(phone);
-phone.calculateAge();
-phone2.calculateAge();
+});
