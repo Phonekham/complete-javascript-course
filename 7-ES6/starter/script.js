70d6709cdf3fa6ced52db4ad7a75f6e128908cfd
Phonekham <phone.madridista@gmail.com>
2019-03-12 20:59:22 +0700

    11. The Spread Operator

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -169,7 +169,7 @@ function Person(name) {
 
 
 //ES5
-var ages = [12, 17, 8, 21, 14, 11];
+// var ages = [12, 17, 8, 21, 14, 11];
 
 // var full = ages.map(function(cur) {
 //   return cur >= 18;
@@ -181,5 +181,38 @@ var ages = [12, 17, 8, 21, 14, 11];
 
 
 //ES6
-console.log(ages.findIndex(cur => cur >= 18));
-console.log(ages.find(cur => cur >= 18));
+// console.log(ages.findIndex(cur => cur >= 18));
+// console.log(ages.find(cur => cur >= 18));
+
+/////////////////////////////////
+// Lecture: Spread operator
+
+
+// function addFourAges(a, b, c, d) {
+//   return a + b + c + d;
+// }
+//
+// var sum1 = addFourAges(18, 30, 12, 21);
+// console.log(sum1);
+
+// //ES5
+// var ages = [18, 30, 12, 21];
+// var sum2 = addFourAges.apply(null, ages);
+// console.log(sum2);
+//
+// //ES6
+// const sum3 = addFourAges(...ages);
+// console.log(sum3);
+//
+//
+// const familySmith = ['John', 'Jane', 'Mark'];
+// const familyMiller = ['Mary', 'Bob', 'Ann'];
+// const bigFamily = [...familySmith, 'Lily', ...familyMiller];
+// console.log(bigFamily);
+//
+//
+const h = document.querySelector('h1');
+const boxes = document.querySelectorAll('.box');
+const all = [h, ...boxes];
+
+Array.from(all).forEach(cur => cur.style.color = 'purple');
