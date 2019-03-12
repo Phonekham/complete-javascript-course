42f81a9e8a555f7c51cf1014f576c2e98083591d
Phonekham <phone.madridista@gmail.com>
2019-03-12 20:04:55 +0700

    9. Destructuring

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -72,13 +72,58 @@ function Person(name) {
 // new Person('John').myFriends5(friends);
 
 // ES6
-var friends = ['Bob', 'Jane', 'Mark'];
-// new Person('John').myFriends5(friends);
-Person.prototype.myFriends6 = function(friends) {
+// var friends = ['Bob', 'Jane', 'Mark'];
+// Person.prototype.myFriends6 = function(friends) {
+//
+//   var arr = friends.map(el => `${this.name} is friends with ${el}`);
+//
+//   console.log(arr);
+// }
+//
+// new Person('Mike').myFriends6(friends);
 
-  var arr = friends.map(el => `${this.name} is friends with ${el}`);
+/////////////////////////////////
+// Lecture: Destructuring
 
-  console.log(arr);
-}
 
-new Person('Mike').myFriends6(friends);
+// ES5
+var john = ['John', 26];
+// var name = john[0];
+// var age = john[1];
+
+
+// ES6
+const [name, age] = ['John', 26];
+console.log(name);
+console.log(age);
+
+const obj = {
+  firstName: 'John',
+  lastName: 'Smith'
+};
+
+const {
+  firstName,
+  lastName
+} = obj;
+console.log(firstName);
+console.log(lastName);
+
+const {
+  firstName: a,
+  lastName: b
+} = obj;
+console.log(a);
+console.log(b);
+
+
+
+// function calcAgeRetirement(year) {
+//     const age = new Date().getFullYear() - year;
+//     return [age, 65 - age];
+// }
+//
+//
+// const [age2, retirement] = calcAgeRetirement(1990);
+// console.log(age2);
+// console.log(retirement);
