27c22e6ca9bce6867de73da003376761986db087
Phonekham <phone.madridista@gmail.com>
2019-03-15 21:18:59 +0700

    12. Building the Search Model

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,13 +1,4 @@
-import axios from 'axios';
-async function getResults(query) {
-  const proxy = 'https://cors-anywhere.herokuapp.com/';
-  const key = '9ae098ee739d17d4ca8dcd11dc6932ef';
-  try {
-    const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-    const recipes = res.data.recipes;
-    console.log(recipes);
-  } catch (error) {
-    alert(error);
-  }
-}
-getResults('pizza');
+import Search from './models/Search';
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
--- 9-forkify/starter/src/js/models/Search.js
+++ 9-forkify/starter/src/js/models/Search.js
@@ -1,1 +1,18 @@
-export default "I'm export string";
+import axios from 'axios';
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+  async getResults() {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = '9ae098ee739d17d4ca8dcd11dc6932ef';
+    try {
+      const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+      // console.log(res);
+      this.result = res.data.recipes;
+      // console.log(this.result);
+    } catch (error) {
+      alert(error);
+    }
+  }
+}
